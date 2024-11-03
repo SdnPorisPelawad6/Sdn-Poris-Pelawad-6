6423eebcfdd398a685073f768785ea1fe03a8e6a
sdn poris pelawad 6 <168566424+SdnPorisPelawad6@users.noreply.github.com>
2024-11-03 17:27:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-        function toggleMenu() {
-        const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger');
         const navLinks = document.querySelector('.nav-links');
 
         hamburger.addEventListener('click', () => {
